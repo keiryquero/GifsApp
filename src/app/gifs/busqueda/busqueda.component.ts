aa0d5ea96c8d09fa254aa2e1796de8c52dd1c501
keiry <keiryquero@gmail.com>
2023-01-15 07:34:18 -0500

    inicio maquetacion,creacion de modulos y componentes del proyecto

---
+++ src/app/gifs/busqueda/busqueda.component.ts
@@ -0,0 +1,20 @@
+import { Component, ElementRef, ViewChild } from '@angular/core';
+import { GifsService } from '../services/gifs.service';
+
+@Component({
+  selector: 'app-busqueda',
+  templateUrl: './busqueda.component.html',
+})
+export class BusquedaComponent {
+
+  @ViewChild('txtBuscar') txtBuscar!:ElementRef<HTMLInputElement>;
+
+  constructor(private gifsServices:GifsService){}
+
+  buscar(){
+    const valor = this.txtBuscar.nativeElement.value;
+    this.gifsServices.buscarGifs(valor);
+    this.txtBuscar.nativeElement.value = " ";
+    }
+
+}
---
+++ src/app/shared/sidebar/sidebar.component.ts
@@ -0,0 +1,18 @@
+import { Component } from '@angular/core';
+
+import { GifsService } from '../../gifs/services/gifs.service';
+
+@Component({
+  selector: 'app-sidebar',
+  templateUrl: './sidebar.component.html',
+
+})
+export class SidebarComponent {
+
+  constructor( private gifsServices:GifsService){}
+
+  get historial(){
+    return this.gifsServices.historial;        
+  }
+  
+}
